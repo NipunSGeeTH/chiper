70a7667790bac3cdd18580f2629968e9d10b2b39
Nipun Sangeeth <sangeethnipun385@gmail.com>
2025-12-01 18:06:37 +0530

    feat:Basic authentication: Login/Logout

--- app/Http/Controllers/Auth/logout.php
+++ app/Http/Controllers/Auth/logout.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-class logout extends Controller
+use Illuminate\Support\Facades\Auth;
+ 
+class Logout extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        //
+        Auth::logout();
+ 
+        // Invalidate session
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+ 
+        return redirect('/')->with('success', 'You have been logged out.');
     }
 }
